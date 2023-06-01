127209526f70035daf6c36b2d2c5463a628526c7
shwethaj1104 <shwethaj1104@gmail.com>
2023-06-01 10:42:15 +0530

    Implemented data jpa relations

---
+++ src/main/java/com/example/demo/students/Student.java
@@ -0,0 +1,41 @@
+package com.example.demo.students;
+
+import com.example.demo.subject.Subject;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    private String name;
+    
+    //to avoid recursive problem
+    @JsonIgnore
+    @ManyToMany(mappedBy = "enrolledStudents")
+    private Set<Subject> subjects = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Subject> getSubjects() {
+        return subjects;
+    }
+
+}
